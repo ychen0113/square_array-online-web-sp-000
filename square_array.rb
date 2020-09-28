5071a5640556a017032b089ebd4a7fe6453d2d67
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-28 16:47:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
   square_array = []
-  x = 0
   array.each do |i|
-    square_array[x] = array[i]**2
-    x += 1
+    square_array[x] << array[i]
   end 
   return square_array
 end
